61e19acde947d9437b88e9afb11a75bd5da3d6f8
Samuel Itwaru <samuelitwaru@gmail.com>
2021-01-01 05:38:42 +0300

    updates

--- app/__init__.py
+++ app/__init__.py
@@ -10,8 +10,8 @@ from rave_python import Rave
 
 app = Flask(__name__)
 
-app.config.from_object(config.DevelopmentConfig)
 app.config.from_object(config.ProductionConfig)
+app.config.from_object(config.DevelopmentConfig)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
